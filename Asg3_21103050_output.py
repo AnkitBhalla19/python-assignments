8edd683dedc10d2703d4d56915db8b3946b39838
AnkitBhalla19 <94590018+AnkitBhalla19@users.noreply.github.com>
2022-02-14 11:37:37 +0530

    Add files via upload

---
+++ Asg3_21103050_output.py
@@ -0,0 +1,83 @@
+QUESTION 1
+ENTER ANY STRING: 
+ankitbhalla
+{'a': 3, 'n': 1, 'k': 1, 'i': 1, 't': 1, 'b': 1, 'h': 1, 'l': 2}
+
+
+QUESTION 2
+ENTER THE DAY: 
+28
+ENTER THE MONTH OF THE YEAR: 
+02
+ENTER THE YEAR: 
+1999
+1 / 3 / 1999
+
+
+QUESTION 3
+Enter elements of a list separated by space 
+3 9 10
+list:  3 9 10
+[(3, 9), (9, 81), (10, 100)]
+
+
+QUESTION 4
+Enter Grade Point: 
+9
+Your Grade is 'A' and Excellent performance
+
+
+QUESTION 5
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+
+
+QUESTION 6
+Enter SID: 
+21103050
+Enter Name: 
+AnkitBhalla
+Do you want to enter another student entry(Y or N):
+Y
+Enter SID: 
+21103009
+Enter Name: 
+XYZ
+Do you want to enter another student entry(Y or N):
+y
+Enter SID: 
+21104077
+Enter Name: 
+PQR
+Do you want to enter another student entry(Y or N):
+n
+<a> {21103050: 'AnkitBhalla', 21103009: 'XYZ', 21104077: 'PQR'}
+<b> {21103050: 'AnkitBhalla', 21104077: 'PQR', 21103009: 'XYZ'}
+<c> {21103009: 'XYZ', 21103050: 'AnkitBhalla', 21104077: 'PQR'}
+Search Name with SID: 
+21103050
+<d> AnkitBhalla
+QUESTION 7
+ENTER THE NUMBER OF TERMS IN THE SERIES: 
+8
+Fibonacci sequence:
+0
+1
+1
+2
+3
+5
+8
+13
+AVERAGE OF RESULTANT FIBONACCI SERIES IS 4.125
+QUESTION 8
+<a> {1, 3, 5, 6, 8}
+<b> {3, 6, 8, 9, 13, 17}
+<c> {1, 2, 4, 5}
+<d> {6, 7, 8, 9, 10}
+<e> {10, 7}
+
