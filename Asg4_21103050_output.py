ac4080f1d9f9e0d625ace8aed4dc047f608de429
AnkitBhalla19 <94590018+AnkitBhalla19@users.noreply.github.com>
2022-02-26 19:23:23 +0530

    Add files via upload

---
+++ Asg4_21103050_output.py
@@ -0,0 +1,54 @@
+ANSWER 1
+Move disk 1 from rod A to rod C
+Move disk 2 from rod A to rod B
+Move disk 1 from rod C to rod B
+Move disk 3 from rod A to rod C
+Move disk 1 from rod B to rod A
+Move disk 2 from rod B to rod C
+Move disk 1 from rod A to rod C
+
+
+ANSWER 2
+Enter the size of pascals triangle 4
+USING LOOPS
+     1 
+    1 1 
+   1 2 1 
+  1 3 3 1 
+
+
+
+USING RECURSSIONS
+      1   
+    1   1   
+  1   2   1   
+1   3   3   1   
+
+
+ANSWER 3
+Enter 2 numbers: 15 40
+a. Checking if the quotient and remainder is a callable value or not.
+False
+False
+<b> The quotient is zero
+<c> Filtered out numbers that are greater than 4 are : [19, 20, 5, 20, 6, 21]
+<d> Set: {5, 6, 19, 20, 21}
+<e> Immutable set: frozenset({19, 20, 5, 6, 21})
+<f> Hash value of the max value from the above set: 21
+
+
+ANSWER 4 
+Object created
+The name of the student it AnkitBhalla and SID is 21103050.
+Object destroyed
+
+
+ANSWER 5
+<a> The updated salary of Mehak is 70000 
+<b>Record of Viren deleted
+
+ANSWER 6
+Enter the word: study
+Enter a new meaningful word using the exact same letters to test your friendship: dusty
+Does the word makes sense?(y/Y or n/N )y
+The friends pass the friendship test
